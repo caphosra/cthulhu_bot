b8ccf47c38d9f9991283999dac1a908cad011c69
Akihisa Yagi <capra314cabra@gmail.com>
2021-02-17 11:16:25 +0900

    Add baseUrl

--- src/@types/dice-expression-evaluator.d.ts
+++ src/@types/dice-expression-evaluator.d.ts
@@ -1,7 +1,7 @@
 //
 // Type file for dice-expression-evaluator.
 //
-declare module 'dice-expression-evaluator' {
+declare module "dice-expression-evaluator" {
     export default class DiceExpression {
         constructor(expr: string);
         roll(): { roll: int, diceSums: int[], diceRaw: any };
